e0873c2e3d820e846941eff964f0a229d15ac532
Romain Dénarié <romain.denarie@exoplatform.com>
2021-10-19 09:40:06 +0200

    Add Rest Service and configuration file

--- services/impl/src/main/java/org/exoplatform/automatic/translation/rest/AutomaticTranslationRestService.java
+++ services/impl/src/main/java/org/exoplatform/automatic/translation/rest/AutomaticTranslationRestService.java
@@ -1,6 +1,9 @@
 package org.exoplatform.automatic.translation.rest;
 
 import io.swagger.annotations.Api;
+import org.exoplatform.automatic.translation.api.AutomaticTranslationService;
+import org.exoplatform.services.log.ExoLogger;
+import org.exoplatform.services.log.Log;
 import org.exoplatform.services.rest.resource.ResourceContainer;
 
 import javax.ws.rs.Path;
@@ -8,5 +11,13 @@ import javax.ws.rs.Path;
 @Path("/automatic-translation")
 @Api(value = "/automatic-translation")
 public class AutomaticTranslationRestService implements ResourceContainer {
+
+  private AutomaticTranslationService automaticTranslationService;
+
+  private static final Log LOG = ExoLogger.getLogger(AutomaticTranslationRestService.class);
+
+  public AutomaticTranslationRestService(AutomaticTranslationService automaticTranslationService) {
+    this.automaticTranslationService = automaticTranslationService;
+  }
 }
 
