d11accddf61f6e51747def576a6806d1ac8f71c6
Romain Dénarié <romain.denarie@exoplatform.com>
2021-11-09 13:01:02 +0100

    Task-50286 : Automatic Translation - Add 3 dots menu for activity and comment
    This commit add the possibility to call translationService to translate an activity or a comment

---
+++ webapps/src/main/webapp/javascript/automatic-translation/extensions.js
@@ -0,0 +1,119 @@
+/*
+ * Copyright (C) 2021 eXo Platform SAS.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+ (function(extensionRegistry,exoi18n) {
+
+
+  function getTranslatedBody(activity) {
+    return activity.translatedBody;
+  }
+
+  function fetchTranslation(content) {
+    return fetch(`${eXo.env.portal.context}/${eXo.env.portal.rest}/automatic-translation/translate`, {
+      headers: {
+       'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      method: 'POST',
+      body: 'message='+encodeURIComponent(content)+'&locale='+eXo.env.portal.language
+    }).then(resp => {
+      if (resp && resp.ok) {
+        return resp.json();
+      } else {
+        throw new Error('Unable to get automatic translation result');
+      }
+    });
+  }
+
+  function initExtensions() {
+    const lang = typeof eXo !== 'undefined' ? eXo.env.portal.language : 'en';
+    const url = `${eXo.env.portal.context}/${eXo.env.portal.rest}/i18n/bundle/locale.portlet.automaticTranslation.automaticTranslationExtension-${lang}.json`;
+    exoi18n.loadLanguageAsync(lang, url);
+    extensionRegistry.registerExtension('activity', 'action', {
+      id: 'translate',
+      isEnabled: (activity, activityTypeExtension) => true,
+      labelKey: 'UIActivity.label.translate',
+      click: (activity, activityTypeExtension) => {
+        const event = {
+          activityId: activity.id,
+        };
+        if (!activity.translatedBody) {
+          fetchTranslation(activityTypeExtension.getBody(activity)).then(translated => {
+            activity.translatedBody = translated.translation;
+            activityTypeExtension.getTranslatedBody = getTranslatedBody;
+            document.dispatchEvent(new CustomEvent('activity-translated',{ detail:event }));
+          });
+        } else {
+          document.dispatchEvent(new CustomEvent('activity-translated',{ detail:event }));
+        }
+      },
+    });
+
+    extensionRegistry.registerExtension('activity', 'comment-action', {
+      id: 'translate',
+      isEnabled: (activity, comment, activityTypeExtension) => true,
+      labelKey: 'UIActivity.label.translate',
+      click: (activity, comment, activityTypeExtension) => {
+        const event = {
+          commentId: comment.id,
+        };
+        if (!comment.translatedBody) {
+          fetchTranslation(activityTypeExtension.getBody(comment)).then(translated => {
+            comment.translatedBody = translated.translation;
+            activityTypeExtension.getCommentTranslatedBody = getTranslatedBody;
+            document.dispatchEvent(new CustomEvent('activity-comment-translated',{ detail:event }));
+          });
+        } else {
+          document.dispatchEvent(new CustomEvent('activity-comment-translated',{ detail:event }));
+        }
+      },
+    });
+
+
+    extensionRegistry.registerComponent('ActivityContent', 'activity-content-extensions', {
+      id: 'translatedBody',
+      vueComponent: Vue.options.components['activity-translated-body'],
+      rank: 1,
+    });
+
+    extensionRegistry.registerComponent('ActivityContent', 'activity-content-extensions', {
+      id: 'commentTranslatedBody',
+      vueComponent: Vue.options.components['activity-comment-translated-body'],
+      rank: 1,
+    });
+  }
+
+  return {
+    init: () => {
+      fetch(`${eXo.env.portal.context}/${eXo.env.portal.rest}/automatic-translation/configuration`, {
+        headers: {
+         'Content-Type': 'application/json'
+        },
+        method: 'GET'
+      }).then(resp => {
+        if (resp && resp.ok) {
+          return resp.json();
+        } else {
+          throw new Error('Unable to get automatic translation configuration');
+        }
+      }).then(data => {
+        if (data.isActive) {
+          initExtensions();
+        }
+      });
+
+    },
+  };
+})(extensionRegistry,exoi18n);
