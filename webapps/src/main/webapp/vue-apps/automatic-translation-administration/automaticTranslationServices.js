2ec38a85911d5ae5bd7efb7baea269f20d2223fd
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2022-04-13 12:32:52 +0100

    TASK-55664 Upgrade ESLint version to use newer ECMAScript version Upgrade ESLint version to use new ECMAScript 2022.

--- webapps/src/main/webapp/vue-apps/automatic-translation-administration/automaticTranslationServices.js
+++ webapps/src/main/webapp/vue-apps/automatic-translation-administration/automaticTranslationServices.js
@@ -16,10 +16,10 @@
  */
 export function getConfiguration() {
   return fetch(`${eXo.env.portal.context}/${eXo.env.portal.rest}/automatic-translation/configuration`, {
-   headers: {
-     'Content-Type': 'application/json'
-   },
-   method: 'GET'
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    method: 'GET'
   }).then(resp => {
     if (resp && resp.ok) {
       return resp.json();
@@ -31,20 +31,20 @@ export function getConfiguration() {
 
 export function setActiveConnector(connector) {
   return fetch(`${eXo.env.portal.context}/${eXo.env.portal.rest}/automatic-translation/setActiveConnector?connector=${connector}`, {
-   headers: {
-     'Content-Type': 'application/json'
-   },
-   method: 'PUT'
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    method: 'PUT'
   });
 }
 
 export function setApiKey(connector,apikey) {
   return fetch(`${eXo.env.portal.context}/${eXo.env.portal.rest}/automatic-translation/setApiKey?connector=${connector}&apikey=${apikey}`,
-   {
-   headers: {
-     'Content-Type': 'application/json'
-   },
-   method: 'PUT'
-  });
+    {
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      method: 'PUT'
+    });
 }
 
