ffa2e9677bec1b2999e94efb6149f72d1fc169a5
Helmi Akermi <70575401+hakermi@users.noreply.github.com>
2023-10-12 13:33:33 +0100

    feat: Add DeepL translate connector - EXO-66770 (#44)
    Add DeepL trasnlate connector
    
    feat: Add java-deepl library to project packaging - EXO-66770 (#45)
    
    Prior to this change, the java-deepl is not included in project packaging as it's needed.
    This PR, adds the library to the project packaging
    
    fix: issue in google translator connector whith text contains single quote char - EXO-66886 (#52)
    
    Prior to this change, when attempting to translate a text with a simple quote char with google translator, the connector fails due to existing quote char in the text due to the use of signle quotes in the request param.
    This prevents this situation by using escaped double quotes in the qurey param.

---
+++ services/impl/src/test/java/org/exoplatform/automatic/translation/impl/connectors/DeepLTranslateConnectorTest.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright (C) 2023 eXo Platform SAS.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.exoplatform.automatic.translation.impl.connectors;
+
+import com.deepl.api.*;
+import org.exoplatform.commons.api.settings.SettingService;
+import org.exoplatform.commons.api.settings.SettingValue;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.Locale;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class DeepLTranslateConnectorTest {
+
+  @Mock
+  private SettingService          settingService;
+
+  @Mock
+  private Translator              translator;
+
+  private DeepLTranslateConnector deepLTranslateConnector;
+
+  @Before
+  public void setUp() {
+    deepLTranslateConnector = new DeepLTranslateConnector(settingService);
+  }
+
+  @Test
+  public void translate() throws NoSuchFieldException, IllegalAccessException, DeepLException, InterruptedException {
+    // Need real auth-key to have a real result
+    TextTranslationOptions textTranslationOptions = new TextTranslationOptions();
+    textTranslationOptions.setTagHandling("html");
+    textTranslationOptions.setSentenceSplittingMode(SentenceSplittingMode.All);
+    TextResult textResult = new TextResult("<h1>Hello</h1>", "fr");
+    Field textTranslationOptionsField = deepLTranslateConnector.getClass().getDeclaredField("textTranslationOptions");
+    textTranslationOptionsField.setAccessible(true);
+    textTranslationOptionsField.set(deepLTranslateConnector, textTranslationOptions);
+    when(translator.translateText("<h1>Bonjour</h1>", null, "en-US", textTranslationOptions)).thenReturn(textResult);
+    Field field = deepLTranslateConnector.getClass().getDeclaredField("translator");
+    field.setAccessible(true);
+    field.set(deepLTranslateConnector, translator);
+    String text = deepLTranslateConnector.translate("<h1>Bonjour</h1>", new Locale("en"));
+    assertNotNull(text);
+    when(translator.translateText("<h1>Bonjour</h1>",
+                                  null,
+                                  "en-US",
+                                  textTranslationOptions)).thenThrow(new DeepLException("error"));
+    text = deepLTranslateConnector.translate("<h1>Bonjour</h1>", new Locale("en"));
+    assertNull(text);
+
+  }
+}
