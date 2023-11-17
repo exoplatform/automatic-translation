62f39b559efafb29164dd31964b6f0e29da71af9
Mohamed Amine Krout <mkrout@exoplatform.com>
2023-11-17 11:45:23 +0100

    feat: Manage translation option by feature - EXO-65580
    feat: Manage translation option by feature - EXO-65580

--- webapps/src/main/webapp/javascript/automatic-translation/extensions.js
+++ webapps/src/main/webapp/javascript/automatic-translation/extensions.js
@@ -48,94 +48,124 @@
     document.dispatchEvent(new CustomEvent('activity-translation-error',{ detail: event }));
   }
 
-  function initExtensions() {
+  function initExtensions(featuresOptions) {
     const lang = typeof eXo !== 'undefined' ? eXo.env.portal.language : 'en';
     const url = `${eXo.env.portal.context}/${eXo.env.portal.rest}/i18n/bundle/locale.portlet.automaticTranslation.automaticTranslationExtension-${lang}.json`;
     exoi18n.loadLanguageAsync(lang, url);
-    extensionRegistry.registerExtension('activity', 'action', {
-      id: 'translate',
-      rank: 9007199254740992,
-      isEnabled: () => true,
-      labelKey: 'UIActivity.label.translate',
-      icon: 'fa-globe-americas',
-      click: (activity, activityTypeExtension) => {
-        const event = {
-          id: activity.id,
-          type: 'activity',
-          spaceId: activity && activity.activityStream && activity.activityStream.space && activity.activityStream.space.id
-        };
-        if (!activity.translatedBody) {
-          fetchTranslation(activityTypeExtension.getBody(activity),event).then(translated => {
-            activity.translatedBody = translated.translation;
-            activityTypeExtension.getTranslatedBody = getTranslatedBody;
+    if (featuresOptions?.streamTranslateShort){
+      extensionRegistry.registerExtension('activity', 'action', {
+        id: 'translate',
+        rank: 9007199254740992,
+        isEnabled: () => true,
+        labelKey: 'UIActivity.label.translate',
+        icon: 'fa-globe-americas',
+        click: (activity, activityTypeExtension) => {
+          const event = {
+            id: activity.id,
+            type: 'activity',
+            spaceId: activity && activity.activityStream && activity.activityStream.space && activity.activityStream.space.id
+          };
+          if (!activity.translatedBody) {
+            fetchTranslation(activityTypeExtension.getBody(activity),event).then(translated => {
+              activity.translatedBody = translated.translation;
+              activityTypeExtension.getTranslatedBody = getTranslatedBody;
+              document.dispatchEvent(new CustomEvent('activity-translated',{ detail: event }));
+            });
+          } else {
             document.dispatchEvent(new CustomEvent('activity-translated',{ detail: event }));
-          });
-        } else {
-          document.dispatchEvent(new CustomEvent('activity-translated',{ detail: event }));
-        }
-      },
-    });
-    extensionRegistry.registerExtension('activity', 'comment-action', {
-      id: 'translate',
-      rank: 9007199254740992,
-      isEnabled: () => true,
-      labelKey: 'UIActivity.label.translate',
-      icon: 'fa-globe-americas',
-      click: (activity, comment, activityTypeExtension) => {
-        const event = {
-          id: comment.id,
-          type: 'comment',
-          spaceId: activity && activity.activityStream && activity.activityStream.space && activity.activityStream.space.id
-        };
-        if (!comment.translatedBody) {
-          fetchTranslation(activityTypeExtension.getBody(comment),event).then(translated => {
-            comment.translatedBody = translated.translation;
-            activityTypeExtension.getCommentTranslatedBody = getTranslatedBody;
+          }
+        },
+      });
+          
+    }
+
+    if (featuresOptions?.streamTranslateComment){
+      extensionRegistry.registerExtension('activity', 'comment-action', {
+        id: 'translate',
+        rank: 9007199254740992,
+        isEnabled: () => true,
+        labelKey: 'UIActivity.label.translate',
+        icon: 'fa-globe-americas',
+        click: (activity, comment, activityTypeExtension) => {
+          const event = {
+            id: comment.id,
+            type: 'comment',
+            spaceId: activity && activity.activityStream && activity.activityStream.space && activity.activityStream.space.id
+          };
+          if (!comment.translatedBody) {
+            fetchTranslation(activityTypeExtension.getBody(comment),event).then(translated => {
+              comment.translatedBody = translated.translation;
+              activityTypeExtension.getCommentTranslatedBody = getTranslatedBody;
+              document.dispatchEvent(new CustomEvent('activity-comment-translated',{ detail: event }));
+            });
+          } else {
             document.dispatchEvent(new CustomEvent('activity-comment-translated',{ detail: event }));
-          });
-        } else {
-          document.dispatchEvent(new CustomEvent('activity-comment-translated',{ detail: event }));
-        }
-      },
-    });
+          }
+        },
+      });
+    }
 
-    extensionRegistry.registerExtension('notes', 'translation-menu-extension', {
-      id: 'notes-auto-translate',
-      rank: 1000,
-      componentOptions: {
-        vueComponent: Vue.options.components['note-automatic-translation'],
-      },
-    });
+    if (featuresOptions?.notesTranslateView){
+      extensionRegistry.registerExtension('notes', 'translation-menu-extension', {
+        id: 'notes-auto-translate',
+        rank: 1000,
+        componentOptions: {
+          vueComponent: Vue.options.components['note-automatic-translation'],
+        },
+      });
+    }
 
-    extensionRegistry.registerExtension('notesEditor', 'translation-extension', {
-      id: 'notes-editor-auto-translate',
-      rank: 1000,
-      componentOptions: {
-        vueComponent: Vue.options.components['note-editor-automatic-translation'],
-      },
-    });
+    if (featuresOptions?.notesTranslateEdition){
+      extensionRegistry.registerExtension('notesEditor', 'translation-extension', {
+        id: 'notes-editor-auto-translate',
+        rank: 1000,
+        componentOptions: {
+          vueComponent: Vue.options.components['note-editor-automatic-translation'],
+        },
+      });
+    }
 
-    extensionRegistry.registerExtension('news', 'translation-menu-extension', {
-      id: 'news-auto-translate',
-      rank: 1000,
-      componentOptions: {
-        vueComponent: Vue.options.components['news-automatic-translation'],
-      },
-    });
+    if (featuresOptions?.newsTranslateView){
+      extensionRegistry.registerExtension('news', 'translation-menu-extension', {
+        id: 'news-auto-translate',
+        rank: 1000,
+        componentOptions: {
+          vueComponent: Vue.options.components['news-automatic-translation'],
+        },
+      });
+    }
 
-    extensionRegistry.registerComponent('ActivityContent', 'activity-content-extensions', {
-      id: 'translatedBody',
-      vueComponent: Vue.options.components['activity-translated-body'],
-      rank: 1,
-    });
+    if (featuresOptions?.streamTranslateShort){
+      extensionRegistry.registerComponent('ActivityContent', 'activity-content-extensions', {
+        id: 'translatedBody',
+        vueComponent: Vue.options.components['activity-translated-body'],
+        rank: 1,
+      });
+    }
 
-    extensionRegistry.registerComponent('ActivityContent', 'activity-content-extensions', {
-      id: 'commentTranslatedBody',
-      vueComponent: Vue.options.components['activity-comment-translated-body'],
-      rank: 1,
-    });
-
+    if (featuresOptions?.streamTranslateComment){
+      extensionRegistry.registerComponent('ActivityContent', 'activity-content-extensions', {
+        id: 'commentTranslatedBody',
+        vueComponent: Vue.options.components['activity-comment-translated-body'],
+        rank: 1,
+      });
+    }
     document.dispatchEvent(new CustomEvent('automatic-translation-extensions-updated'));
+  }
+
+  function getFeaturesOptions() {
+    return fetch(`${eXo.env.portal.context}/${eXo.env.portal.rest}/automatic-translation/getFeaturesOptions`, {
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      method: 'GET'
+    }).then(resp => {
+      if (resp && resp.ok) {
+        return resp.json();
+      } else {
+        throw new Error('Unable to get automatic translation Features Options');
+      }
+    });   
   }
 
   return {
@@ -153,7 +183,19 @@
         }
       }).then(result => {
         if (result === 'true') {
-          initExtensions();
+          getFeaturesOptions().then(data => {
+            let featuresOptions = {
+              streamTranslateShort: true,
+              streamTranslateComment: true,
+              newsTranslateView: true,
+              notesTranslateEdition: true,
+              notesTranslateView: true,
+            };
+            if (data){
+              featuresOptions = data;
+            }
+            initExtensions(featuresOptions);
+          });
         }
       });
     },
